8b7a317cd0e7a5c40b3f49170c53c193c631fa33
Matthew Perrot <Matthew@Matthews-MacBook-Air.local>
2022-10-27 16:23:47 -0700

    commit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,11 +2,12 @@ import GalleryItem from './GalleryItem'
 
 function Gallery(props){
 
-    const display = props.data.map((item,index) => {
+    const display = props.data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
     })
+
     return (
         <div>
             {display}
